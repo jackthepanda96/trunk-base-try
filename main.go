c987de6f6df38fabab49859e87cb200b25a2938e
Jerry Young <jerryyoung2911@gmail.com>
2021-12-29 23:37:27 +0700

    after commit

--- main.go
+++ main.go
@@ -7,12 +7,9 @@ func main() {
 	fmt.Println("Update 1")
 	fmt.Println("Update 2")
 	fmt.Println("Update 3")
-<<<<<<< HEAD
 	fmt.Println("Update 4")
 	fmt.Println("Update 5")
-=======
 	fmt.Println("Fitur 1-1")
 	fmt.Println("Fitur 1-2")
 	fmt.Println("Fitur 1-3")
->>>>>>> fitur1
 }
